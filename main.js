c554db986cc8683b33a77d85dc3b83ee2f5d8a03
Muhammad Hasan Bin Suwandi <hasan@mhbs.me>
2026-03-06 15:28:00 +0800

    Added test routes

--- main.js
+++ main.js
@@ -4,9 +4,18 @@ import 'dotenv/config'
 import './redis.js'
 
 const app = express()
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World\n')
+})
+
+app.get("/get/:testId", (req, res) => {
+  res.send(`Data received: ${req.body.testId}\n`)
+})
+
+app.post('/post', (req, res) => {
+  res.send('Data received\n')
 })
 
 app.listen(3000, () => {
