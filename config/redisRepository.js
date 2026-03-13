58fc2138a62fd3e78a77fe23a179e369a76080fd
Muhammad Hasan Bin Suwandi <hasan@mhbs.me>
2026-03-13 14:57:40 +0800

    Bugfix redis repo code

--- config/redisRepository.js
+++ config/redisRepository.js
@@ -1,5 +1,8 @@
 import { Repository } from "redis-om";
-import redisClient from "./redis";
+import redisClient from "./redis.js";
+import humiditySchema from "../models/humidity.js";
+import temperatureSchema from "../models/temperature.js";
+import motionSchema from "../models/motion.js";
 
 const humidityRepository = new Repository(humiditySchema, redisClient);
 const temperatureRepository = new Repository(temperatureSchema, redisClient);
